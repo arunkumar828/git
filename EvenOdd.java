3b833e3317243e9c2e59f52fd79862481d0a5a63
arun kumar <kashyaparun828@gmail.com>
2022-11-01 19:05:41 +0530

    2nd commit

--- EvenOdd.java
+++ EvenOdd.java
@@ -3,16 +3,13 @@ public class EvenOdd{
     public static void main(String[] args){
         Scanner obj=new Scanner (System.in);
         int n;
-        System.out.print("Enter the no.: ");
+        System.out.print("Enter the number to find even or odd.: ");
         n=obj.nextInt();
 
-    if (n%2==0){
+    if (n%2==0)
         System.out.println(n+ " is a even number");
-    }
     else
-    {
         System.out.println(n+ " is a Odd number");
-    }
     obj.close();
 }
 }
