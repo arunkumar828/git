668eb5e3721e3c735d569079738d4c8f508efac8
arun kumar <kashyaparun828@gmail.com>
2022-11-01 18:53:58 +0530

    create EvenOdd check code

---
+++ EvenOdd.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class EvenOdd{
+    public static void main(String[] args){
+        Scanner obj=new Scanner (System.in);
+        int n;
+        System.out.print("Enter the no.: ");
+        n=obj.nextInt();
+
+    if (n%2==0){
+        System.out.println(n+ " is a even number");
+    }
+    else
+    {
+        System.out.println(n+ " is a Odd number");
+    }
+    obj.close();
+}
+}
